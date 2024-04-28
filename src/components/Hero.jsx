bda665e23a6f2eb27759436a5865aaea28745778
Kunal Prajapati <40620699+KunalPrajapati@users.noreply.github.com>
2024-04-28 15:57:03 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
       <h1 className='head_text'> Summarize Articles with <br className='max-md:hidden'/>
       <span className='orange_gradient'>OpeanAI GPT-4</span> </h1>
       <h2 className='desc'>
-        Simplify your reading with BlogBrief. an Open-source article summarizer that transforms lengthy articles into clear and concise summaries
+        Simplify your reading with Smart Summarizer. an Open-source article summarizer that transforms lengthy articles into clear and concise summaries
       </h2>
     </header>
   )
